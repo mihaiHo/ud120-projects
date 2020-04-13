7f14b36d1fd6bb4ee7d83691578cc6faac1684c9
mihaiHo <mihaihopirtean@yahoo.com>
2020-04-13 19:00:13 +0200

    completeing svm with jupyter

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -24,7 +24,23 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+### features_train = features_train[:len(features_train)//100]
+### labels_train = labels_train[:len(labels_train)//100]
 
+from sklearn.svm import SVC
+
+clf = SVC(C = 10**4, kernel="rbf")
+t0 = time()
+clf.fit(features_train,labels_train)
+print('training time:', round(time()-t0, 3), 's')
+
+t1 = time()
+pred = clf.predict(features_test)
+print('prediction time:', round(time()-t1, 3), 's')
+
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(pred, labels_test)
+print('accuracy: ',acc)
 #########################################################
 
 
